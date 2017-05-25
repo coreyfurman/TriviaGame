cb0009bf1ccf09e59c2da6d754c46896b4550169
coreyfurman <cfurm14@gmail.com>
2017-05-25 17:54:17 -0400

    Shark Triv

--- app.js
+++ app.js
@@ -1,22 +1,57 @@
 window.onload = function() {
   $("#start").on("click", timeTil.start);
+  $("#theresults").hide();
+  $('#qs').hide();
+  $("#done").hide();
 };
 
 var timeoutId;
+var clockRunning = false;
+var unanswered = 5;
 var timeTil = {
 
   time: 60, 
 
   start: function() {
-
+    $('#qs').show();
+    $('#done').show();
+    $('#start').hide();
+    
     // DONE: Use setInterval to start the count here and set the clock to running.
-        timeoutId = setTimeout(timeTil.count, 1000);
+        if (clockRunning == false) {
+
+          timeoutId = setTimeout(timeTil.count, 1000);
+          clockRunning = true;
+        }
+        
+
+      $("#done").on("click", timeTil.done);
+      
+  },
+
+
+  // if id selected is equal to #correctradioquestion1 then correct goes up by 1. same thing for x y z. 
+  //else (not equal to #correctradioquestion1 then)  incorrect goes up by 1.
+
+ done: function() {
+   $('#qs').hide();
+   $('#theresults').show();
       
   },
   count: function() {
 
     // DONE: increment time by 1, remember we cant use "this" here.
     timeTil.time--;
+
+  
+    if (timeTil.time != 0){
+      timeoutId = setTimeout(timeTil.count, 1000);
+    }
+    else if (timeTil.time == 0) {
+          $("#qs").hide();
+          $("#done").hide();
+          $("#theresults").show();
+    }
 
     // DONE: Get the current time, pass that into the stopwatch.timeConverter function,
     //       and save the result in a variable.
@@ -45,4 +80,55 @@ var timeTil = {
     return minutes + ":" + seconds;
   }
 };
+
+
+
+
+var correct = 0;
+var wrong = 0;
+
+
+
+$(".right1").click(function(){
+correct++;
+document.querySelector("#correct-answers").innerHTML = correct;
+});
+
+$(".right2").click(function(){
+correct++;
+document.querySelector("#correct-answers").innerHTML = correct;
+});
+
+$(".right3").click(function(){
+correct++;
+document.querySelector("#correct-answers").innerHTML = correct;
+});
+
+$(".right4").click(function(){
+correct++;
+document.querySelector("#correct-answers").innerHTML = correct;
+});
+
+
+$(".wrong1").click(function(){
+wrong++;
+document.querySelector("#incorrect-answers").innerHTML = wrong;
+});
+$(".wrong2").click(function(){
+wrong++;
+document.querySelector("#incorrect-answers").innerHTML = wrong;
+});
+$(".wrong3").click(function(){
+wrong++;
+document.querySelector("#incorrect-answers").innerHTML = wrong;
+});
+$(".wrong4").click(function(){
+wrong++;
+document.querySelector("#incorrect-answers").innerHTML = wrong;
+});
+$(".wrong5").click(function(){
+wrong++;
+document.querySelector("#incorrect-answers").innerHTML = wrong;
+});
+
 
